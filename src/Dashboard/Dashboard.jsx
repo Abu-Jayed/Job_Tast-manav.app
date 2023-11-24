e73ea2c381c5be98ab964d089bee4d4bafb14fbb
Abu Jayed <aj.webdevlopment@gmail.com>
2023-11-24 09:55:27 +0600

    dashboard is now privet protected route.

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,11 +1,22 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../AuthProvider/AuthProvider";
 
 const Dashboard = () => {
-  
+  const { user } = useContext(AuthContext);
 
+  if (!user) {
+    return <Navigate to={"/login"}></Navigate>;
+  }
   return (
     <>
       <h1>This is our Dashboard</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis enim repudiandae quas voluptate deleniti odio facilis. Expedita sapiente architecto sit voluptates iure. Cum nesciunt inventore beatae velit voluptatibus aut aspernatur.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis enim
+        repudiandae quas voluptate deleniti odio facilis. Expedita sapiente
+        architecto sit voluptates iure. Cum nesciunt inventore beatae velit
+        voluptatibus aut aspernatur.
+      </p>
     </>
   );
 };
