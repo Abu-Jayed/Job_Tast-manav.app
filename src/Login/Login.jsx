6b7ba6dec546389128a941b181ab34311de65a41
Abu Jayed <aj.webdevlopment@gmail.com>
2023-11-24 10:42:30 +0600

    login form functionality added.

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,17 +1,27 @@
 const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+  };
   return (
     <div className="max-w-6xl">
       <img
         src="/src/assets/wave.png"
         className="fixed hidden lg:block inset-0 h-full"
-        style={{zIndex: -1}}
+        style={{ zIndex: -1 }}
       />
       <div className="max-w-6xl mx-auto h-screen flex flex-col justify-center items-center lg:grid lg:grid-cols-2">
         <img
           src="/src/assets/unlock.svg"
           className="hidden lg:block w-1/2 hover:scale-150 transition-all duration-500 transform mx-auto"
         />
-        <form className="flex flex-col justify-center items-center w-1/2">
+        <form
+          onSubmit={handleLogin}
+          className="flex flex-col justify-center items-center w-1/2"
+        >
           <img src="/src/assets/avatar.svg" className="w-28" />
           <h2 className="my-8 font-display font-bold text-3xl text-gray-700 text-center">
             Welcome to you
@@ -19,7 +29,9 @@ const Login = () => {
           <div className="relative">
             <i className="fa fa-user absolute text-primarycolor text-xl"></i>
             <input
+              name="email"
               type="email"
+              required
               placeholder="email"
               className="pl-8 border-b-2 font-display focus:outline-none focus:border-primarycolor transition-all duration-500 capitalize text-lg"
             />
@@ -27,6 +39,8 @@ const Login = () => {
           <div className="relative mt-8">
             <i className="fa fa-lock absolute text-primarycolor text-xl"></i>
             <input
+              required
+              name="password"
               type="password"
               placeholder="password"
               className="pl-8 border-b-2 font-display focus:outline-none focus:border-primarycolor transition-all duration-500 capitalize text-lg"
@@ -35,12 +49,12 @@ const Login = () => {
           <a href="#" className="self-end mt-4 text-gray-600 font-bold">
             Forgot password?
           </a>
-          <a
-            href="#"
-            className="py-3 px-20 bg-primarycolor rounded-full text-white font-bold uppercase text-lg mt-4 transform hover:translate-y-1 transition-all duration-500"
+          <button
+            type="submit"
+            className="py-3 px-20 bg-[#F9A826] rounded-full text-black font-bold uppercase text-lg mt-4 transform hover:translate-y-1 transition-all duration-500"
           >
             Login
-          </a>
+          </button>
         </form>
       </div>
     </div>
