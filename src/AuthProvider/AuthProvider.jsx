7b8a25188db949310eb384a717fcf7a12cd7e236
Abu Jayed <aj.webdevlopment@gmail.com>
2023-11-24 09:44:08 +0600

    auth context  created to know if user login or not.

---
+++ src/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { createContext } from "react";
+
+export const AuthContext = createContext();
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(false);
+
+  const authInfo = {
+    user,
+    setUser,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
 import Dashboard from "./Dashboard/Dashboard.jsx";
 import Home from "./Home/Home.jsx";
+import "./index.css";
 import Login from "./Login/Login.jsx";
+import AuthProvider from "./AuthProvider/AuthProvider.jsx";
 
 const router = createBrowserRouter([
   {
@@ -21,6 +22,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
